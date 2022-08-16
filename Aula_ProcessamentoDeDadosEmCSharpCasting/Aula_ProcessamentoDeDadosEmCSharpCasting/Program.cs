07be0b66baefcfa890f78452abcb3629864c67f2
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-08-15 21:48:17 -0300

    Aula processamento de dados

---
+++ Aula_ProcessamentoDeDadosEmCSharpCasting/Aula_ProcessamentoDeDadosEmCSharpCasting/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Aula_ProcessamentoDeDadosEmCSharpCasting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a;
+            int b;
+
+            a = 5;
+            b = (int)a;
+
+            Console.WriteLine(b)
+        }
+    }
+} 
