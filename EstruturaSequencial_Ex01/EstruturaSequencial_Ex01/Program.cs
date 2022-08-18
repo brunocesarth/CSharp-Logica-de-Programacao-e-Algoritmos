a85841d6435df53187c8e8b50f4d067f47fc07c0
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-08-18 10:51:45 -0300

    Estrutura sequencial - Exercício 01

---
+++ EstruturaSequencial_Ex01/EstruturaSequencial_Ex01/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace EstruturaSequencial_Ex01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double largura, comprimento, precoMetroQuadrado, area, preco;
+
+            largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            comprimento = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            precoMetroQuadrado = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            area = largura * comprimento;
+            preco = area * precoMetroQuadrado;
+
+            Console.WriteLine("Area = " + area.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Preço = " + preco.ToString("F2", CultureInfo.InvariantCulture));
+         
+            Console.ReadLine();
+        }
+    }
+}
