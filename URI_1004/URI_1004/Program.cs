9fea0bf07ba4db983078c71545ce42fbf353d06c
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-08-20 15:01:44 -0300

    URI_1004

---
+++ URI_1004/URI_1004/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace URI_1004
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N1, N2, PROD;
+            N1 = int.Parse(Console.ReadLine());
+            N2 = int.Parse(Console.ReadLine());
+            PROD = N1 * N2;
+            Console.WriteLine("PROD = " + PROD);
+        }
+    }
+}
