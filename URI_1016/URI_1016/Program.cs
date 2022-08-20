bf4514e23f586bb1178b1fe1726a34f44477c4fc
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-08-20 19:57:32 -0300

    URI_1016

---
+++ URI_1016/URI_1016/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace URI_1016
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int minutos, km;
+            km = int.Parse(Console.ReadLine());
+            minutos = km * 2;
+            Console.WriteLine(minutos + " minutos");
+        }
+    }
+}
