76b9e77536fbe54a4c6f1a7835e2586dbcccc310
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-08-20 16:55:43 -0300

    URI_1008

---
+++ URI_1008/URI_1008/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace URI_1008
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroFunc, hrTrabalhadas;
+            double valorHora, salario;
+            numeroFunc = int.Parse(Console.ReadLine());
+            hrTrabalhadas = int.Parse(Console.ReadLine());
+            valorHora = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            salario = valorHora * hrTrabalhadas;
+            Console.WriteLine("NUMBER = " + numeroFunc);
+            Console.WriteLine("SALARY = U$ " + salario.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
