d35c43b140079026cfd2c9e35ede02aea84d98d3
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-08-20 20:26:56 -0300

    URI_1017

---
+++ URI_1017/URI_1017/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+namespace URI_1017
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tempoViagem, velocidadeMedia, distanciaPercorrida;
+            double litrosTotais;
+            tempoViagem = int.Parse(Console.ReadLine());
+            velocidadeMedia = int.Parse(Console.ReadLine());
+            distanciaPercorrida = tempoViagem * velocidadeMedia;
+            litrosTotais = distanciaPercorrida / 12.0;
+            Console.WriteLine(litrosTotais.ToString("F3", CultureInfo.InvariantCulture));
+        }
+    }
+}
