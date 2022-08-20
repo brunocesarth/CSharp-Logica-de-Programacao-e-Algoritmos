4650697452bba798d957dcc05ace1b61825f308d
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-08-20 19:26:58 -0300

    URI_1014

---
+++ URI_1014/URI_1014/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+namespace URI_1014
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int X;
+            double Y, total;
+
+            X = int.Parse(Console.ReadLine());
+            Y = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            total = X / Y;
+            Console.WriteLine(total.ToString("F3", CultureInfo.InvariantCulture) + " km/l");
+        }
+    }
+}
