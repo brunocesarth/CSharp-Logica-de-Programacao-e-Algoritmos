14c42f7ba56d20579972dde790a4f5e8e866c8a8
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-08-20 21:18:49 -0300

    Estrutura Sequencial - Exercício 02

---
+++ EstrSequencial_Ex02/EstrSequencial_Ex02/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace EstrSequencial_Ex02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double baseRetangulo, alturaRetangulo, area, perimetro, diagonal;
+
+            baseRetangulo = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            alturaRetangulo = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            area = baseRetangulo * alturaRetangulo;
+            perimetro = (baseRetangulo + alturaRetangulo) * 2;
+            diagonal = Math.Sqrt((Math.Pow(baseRetangulo, 2) + Math.Pow(alturaRetangulo, 2)));
+
+            Console.WriteLine("AREA = " + area.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("PERIMETRO = " + perimetro.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("DIAGONAL = " + diagonal.ToString("F4", CultureInfo.InvariantCulture));
+        }
+    }
+}
