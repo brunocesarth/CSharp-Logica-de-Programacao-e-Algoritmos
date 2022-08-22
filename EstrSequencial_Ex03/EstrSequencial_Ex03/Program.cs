25eaac77dc7cfa56d393e287e991d6e97fca5456
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-08-22 09:53:48 -0300

    Estrutura Sequencial - Exercício 03

---
+++ EstrSequencial_Ex03/EstrSequencial_Ex03/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace EstrSequencial_Ex03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double media;
+            string[] pessoa = Console.ReadLine().Split(' ');
+            string nome1 = pessoa[0];
+            int idade1 = int.Parse(pessoa[1], CultureInfo.InvariantCulture);
+
+            pessoa = Console.ReadLine().Split(' ');
+            string nome2 = pessoa[0];
+            int idade2 = int.Parse(pessoa[1], CultureInfo.InvariantCulture);
+
+            media = (double) (idade1 + idade2) / 2.0;
+
+            Console.WriteLine("A idade média de " + nome1 + " e " + nome2 + " é de " + media.ToString("F1", CultureInfo.InvariantCulture) + " anos");
+        }
+    }
+}
