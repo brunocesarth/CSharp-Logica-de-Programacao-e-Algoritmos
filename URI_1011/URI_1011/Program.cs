2d99f6b7733aee69fb48db6277e1eb65a1843aa7
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-08-22 10:40:40 -0300

    URI_1011

---
+++ URI_1011/URI_1011/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+
+namespace URI_1011
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double raio, volume, PI;
+            PI = 3.14159;
+            raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            volume =(double) (4 / 3.0) * PI * Math.Pow(raio, 3);
+            Console.WriteLine("VOLUME = " + volume.ToString("F3", CultureInfo.InvariantCulture));
+        }
+    }
+}
