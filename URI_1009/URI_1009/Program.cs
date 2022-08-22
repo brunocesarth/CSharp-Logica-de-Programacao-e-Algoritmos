fb8682c55d7d6180446513cb39250b856a5bc241
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-08-22 10:24:12 -0300

    URI_1009

---
+++ URI_1009/URI_1009/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+namespace URI_1009
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+            double salarioFixo, vendas, total;
+            nome = Console.ReadLine();
+            salarioFixo = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            vendas = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            total = salarioFixo + ((vendas/100)*15);
+            Console.WriteLine("TOTAL = R$ " + total.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
