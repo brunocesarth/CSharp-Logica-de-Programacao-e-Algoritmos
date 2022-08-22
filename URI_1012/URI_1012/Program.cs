3083d21f0cf23b400213a2e2d38a7c8363cfc241
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-08-22 11:50:32 -0300

    Update Program.cs

--- URI_1012/URI_1012/Program.cs
+++ URI_1012/URI_1012/Program.cs
@@ -13,11 +13,13 @@ namespace URI_1012
             A = double.Parse(vet[0], CultureInfo.InvariantCulture);
             B = double.Parse(vet[1], CultureInfo.InvariantCulture);
             C = double.Parse(vet[2], CultureInfo.InvariantCulture);
+
             trianguloRetangulo = (double)(A * C) / 2;
             circuloDeRaio = (double)PI * Math.Pow(C, 2);
             areaTrapezio = (double)((A + B) * C) / 2 ;
             areaQuadrado = B * B;
             areaRetangulo = A * B;
+
             Console.WriteLine("TRIANGULO: " + trianguloRetangulo.ToString("F3", CultureInfo.InvariantCulture));
             Console.WriteLine("CIRCULO: " + circuloDeRaio.ToString("F3", CultureInfo.InvariantCulture));
             Console.WriteLine("TRAPEZIO: " + areaTrapezio.ToString("F3", CultureInfo.InvariantCulture));
