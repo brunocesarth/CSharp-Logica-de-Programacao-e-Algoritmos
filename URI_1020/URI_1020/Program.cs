65ade18a4f7faede527ab719a17b82570537a84d
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-08-24 20:22:53 -0300

    URI_1020

---
+++ URI_1020/URI_1020/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace URI_1020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor, ano, mes, dia, resto;
+            valor = int.Parse(Console.ReadLine());
+            ano = valor / 365;
+            resto = valor % 365;
+            mes = resto / 30;
+            dia = resto % 30;
+
+            Console.WriteLine(ano + " ano(s)");
+            Console.WriteLine(mes + " mes(es)");
+            Console.WriteLine(dia + " dia(s)");
+        }
+    }
+}
