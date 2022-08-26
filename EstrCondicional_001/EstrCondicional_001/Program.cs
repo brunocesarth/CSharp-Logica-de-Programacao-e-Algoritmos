142cf8207c02ba255a3e4ffcd5cec0abbdc37150
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-08-26 20:36:57 -0300

    Estrutura condicional

---
+++ EstrCondicional_001/EstrCondicional_001/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace EstrCondicional_001 {
+    class Program {
+        static void Main(string[] args) {
+            int x = 5;
+            Console.WriteLine("Bom dia!");
+
+            if (x < 0) {
+                Console.WriteLine("Boa tarde!");
+            }
+
+            Console.WriteLine("Boa noite!");
+
+            Console.ReadLine();
+        }
+    }
+}
