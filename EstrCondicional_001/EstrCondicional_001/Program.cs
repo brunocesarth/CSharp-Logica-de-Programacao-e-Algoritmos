92d79be7fa818cf885784c072cb4d0521477fb1c
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-08-27 17:13:23 -0300

    Update Program.cs

--- EstrCondicional_001/EstrCondicional_001/Program.cs
+++ EstrCondicional_001/EstrCondicional_001/Program.cs
@@ -12,6 +12,18 @@ namespace EstrCondicional_001 {
 
             Console.WriteLine("Boa noite!");
 
+            int horas;
+            Console.Write("Quantas horas?");
+            horas = int.Parse(Console.ReadLine());
+
+            if (horas < 12) {
+                Console.WriteLine("Bom dia!");
+            } else if (horas < 18) {
+                Console.WriteLine("Boa tarde!");
+            } else {
+                Console.WriteLine("Boa noite!");
+            }
+
             Console.ReadLine();
         }
     }
