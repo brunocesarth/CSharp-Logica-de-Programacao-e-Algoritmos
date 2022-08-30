ab74c87ac06736a777357c9de24c3f94be3b013e
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-08-30 09:50:38 -0300

    URI_1044

---
+++ URI_1044/URI_1044/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace URI_1044 {
+    class Program {
+        static void Main(string[] args) {
+            int n1, n2;
+            string[] vetor = Console.ReadLine().Split(' ');
+            n1 = int.Parse(vetor[0]);
+            n2 = int.Parse(vetor[1]);
+
+            if (n1 % n2 == 0 || n2 % n1 == 0) {
+                Console.WriteLine("Sao Multiplos");
+            } else {
+                Console.WriteLine("Nao sao Multiplos");
+            }
+        }
+    }
+}
