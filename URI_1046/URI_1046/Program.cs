ad1e21d80fa4010a919a8d2619a8ce9e05fb4958
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-08-30 10:14:21 -0300

    URI_1046

---
+++ URI_1046/URI_1046/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace URI_1046 {
+    class Program {
+        static void Main(string[] args) {
+            int n1, n2, totalHoras = 0;
+            string[] vet = Console.ReadLine().Split(' ');
+            n1 = int.Parse(vet[0]);
+            n2 = int.Parse(vet[1]);
+
+            if (n1 < n2) {
+                totalHoras = n2 - n1;
+            } else if (n2 < n1) {
+                totalHoras = (24 - n1) + n2;
+            } else {
+                totalHoras = 24;
+            }
+            Console.WriteLine("O JOGO DUROU " + totalHoras + " HORA(S)");
+        }
+    }
+}
