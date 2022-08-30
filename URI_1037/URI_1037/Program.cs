3749b466a4ca47066964ce669656daa2d675a632
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-08-30 20:37:44 -0300

    URI_1037

---
+++ URI_1037/URI_1037/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace URI_1037 {
+    class Program {
+        static void Main(string[] args) {
+            double n = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            if (n >= 0 && n <= 25.00) {
+                Console.WriteLine("Intervalo [0,25]");
+            } else if (n > 25.00 && n <= 50.00) {
+                Console.WriteLine("Intervalo (25,50]");
+            } else if (n > 50.00 && n <= 75.00) {
+                Console.WriteLine("Intervalo (50,75]");
+            } else if (n > 75.00 && n <= 100.00) {
+                Console.WriteLine("Intervalo (75,100]");
+            } else {
+                Console.WriteLine("Fora de intervalo");
+            }
+        }
+    }
+}
