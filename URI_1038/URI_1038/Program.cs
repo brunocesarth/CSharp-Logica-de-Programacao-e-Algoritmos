6a7fbe8206cfcab4d829539d3be11634e48a8c27
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-08-30 09:31:40 -0300

    URI_1038

---
+++ URI_1038/URI_1038/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+
+namespace URI_1038 {
+    class Program {
+        static void Main(string[] args) {
+            int item1, item2;
+            double valor = 0;
+            string[] vetor = Console.ReadLine().Split(' ');
+            item1 = int.Parse(vetor[0]);
+            item2 = int.Parse(vetor[1]);
+
+            switch (item1) {
+                case 1:
+                    valor += 4.00 * item2;
+                    break;
+                case 2:
+                    valor += 4.50 * item2;
+                    break;
+                case 3:
+                    valor += 5.00 * item2;
+                    break;
+                case 4:
+                    valor += 2.00 * item2;
+                    break;
+                case 5:
+                    valor += 1.50 * item2;
+                    break;
+            }
+            Console.WriteLine("Total: R$ " + valor.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
