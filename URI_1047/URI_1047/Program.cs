eae58d6c33ace2fc19de37779c6b8679a33d024a
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-09-01 11:13:33 -0300

    URI_1047

---
+++ URI_1047/URI_1047/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace URI_1047 {
+    class Program {
+        static void Main(string[] args) {
+            int horaInicial, minutoInicial, horaFinal, minutoFinal, horas = 0, minutos = 0, resto, tempoTotal = 0;
+            string[] vet = Console.ReadLine().Split(' ');
+            horaInicial = int.Parse(vet[0]);
+            minutoInicial = int.Parse(vet[1]);
+            horaFinal = int.Parse(vet[2]);
+            minutoFinal = int.Parse(vet[3]);
+
+            if (horaInicial == horaFinal && minutoInicial == minutoFinal) {
+                horas = 24;
+            } else if (horaInicial < horaFinal) {
+                tempoTotal = ((horaFinal - horaInicial) * 60) + (minutoFinal - minutoInicial);
+            }
+            Console.WriteLine("O JOGO DUROU " + horas + " HORA(S) E " + minutos + " MINUTO(S)");
+        }
+    }
+}
