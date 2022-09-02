c5c4f95dbaa55da75160986739e4e9c0b4913d3c
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-09-02 16:50:10 -0300

    Update Program.cs

--- URI_1051/URI_1051/Program.cs
+++ URI_1051/URI_1051/Program.cs
@@ -4,7 +4,7 @@ using System.Globalization;
 namespace URI_1051 {
     class Program {
         static void Main(string[] args) {
-            double renda, valorImposto, valorImpostoDois;
+            double renda, valorImposto;
             renda = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
             if (renda < 2000.00) {
@@ -13,14 +13,11 @@ namespace URI_1051 {
                 valorImposto = ((renda - 2000.00) / 100.00) * 8.00;
                 Console.WriteLine("R$ " + valorImposto.ToString("F2", CultureInfo.InvariantCulture));
             } else if (renda >= 3000.01 && renda <= 4500.00) {
-                valorImposto = ((renda - 2000.00 - (renda % 1000.00)) / 100.00 * 8.00) + (((renda % 1000.00) / 100.00) * 18.00);
+                valorImposto = (((renda - 3000.00) / 100) * 18.00) +((1000.00 / 100) * 8.00);
                 Console.WriteLine("R$ " + valorImposto.ToString("F2", CultureInfo.InvariantCulture));
             } else {
-                valorImposto = (renda - 4500);
-                valorImpostoDois = (valorImposto /100) * 28.00;
-                Console.WriteLine(valorImposto);
-                Console.WriteLine(valorImpostoDois);
-
+                valorImposto = ((renda - 4500) / 100 * 28.00) + ((1500.00 / 100) * 18.00) + ((1000 / 100) * 8.00);
+                Console.WriteLine("R$ " + valorImposto.ToString("F2", CultureInfo.InvariantCulture));
             }
         }
     }
