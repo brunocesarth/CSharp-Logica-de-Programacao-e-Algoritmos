3be5e83ea455559a3c6359b8c56c329885e1a3dd
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-09-02 19:45:13 -0300

    URI_1065

---
+++ URI_1065/URI_1065/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+
+namespace URI_1065 {
+    class Program {
+        static void Main(string[] args) {
+            double n1, n2, n3, n4, n5, contador = 0;
+            n1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            n2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            n3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            n4 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            n5 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            if (n1 % 2 == 0) {
+                contador += 1;
+            }
+            if (n2 % 2 == 0) {
+                contador += 1;
+            }
+            if (n3 % 2 == 0) {
+                contador += 1;
+            }
+            if (n4 % 2 == 0) {
+                contador += 1;
+            }
+            if (n5 % 2 == 0) {
+                contador += 1;
+            }
+
+            Console.WriteLine(contador + " valores pares");
+        }
+    }
+}
