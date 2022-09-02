f7947c8b4ecb3165c370a3ddf63081bd41486d50
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-09-02 20:04:21 -0300

    URI_1070

---
+++ URI_1070/URI_1070/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace URI_1070 {
+    class Program {
+        static void Main(string[] args) {
+            int n = int.Parse(Console.ReadLine());
+            if (n % 2 == 0) {
+                n += 1;
+                Console.WriteLine(n);
+                for(int i = 0; i <= 4; i++) {
+                    n += 2;
+                    Console.WriteLine(n);
+                }
+            } else {
+                Console.WriteLine(n);
+                for (int i = 0; i <= 4; i++) {
+                    n += 2;
+                    Console.WriteLine(n);
+                }
+            }
+        }
+    }
+}
