3b4a94b88b00ec96d143dfa494284868526457f6
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-09-02 18:16:07 -0300

    URI_1050

---
+++ URI_1050/URI_1050/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace URI_1050 {
+    class Program {
+        static void Main(string[] args) {
+            int n = int.Parse(Console.ReadLine());
+            string cidade;
+            switch (n) {
+                case 61:
+                    cidade = "Brasilia";
+                    break;
+                case 71:
+                    cidade = "Salvador";
+                    break;
+                case 11:
+                    cidade = "Sao Paulo";
+                    break;
+                case 21:
+                    cidade = "Rio de Janeiro";
+                    break;
+                case 32:
+                    cidade = "Juiz de Fora";
+                    break;
+                case 19:
+                    cidade = "Campinas";
+                    break;
+                case 27:
+                    cidade = "Vitoria";
+                    break;
+                case 31:
+                    cidade = "Belo Horizonte";
+                    break;
+                default:
+                    cidade = "DDD nao cadastrado";
+                    break;
+            }
+            Console.WriteLine(cidade);
+        }
+    }
+}
