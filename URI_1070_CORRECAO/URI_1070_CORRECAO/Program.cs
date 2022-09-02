d1a1a36f93d75bd75f2193a6cfe114d578b988ee
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-09-02 20:07:54 -0300

    URI_1070_CORRECAO

---
+++ URI_1070_CORRECAO/URI_1070_CORRECAO/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace URI_1070_CORRECAO {
+    class Program {
+        static void Main(string[] args) {
+            int n = int.Parse(Console.ReadLine());
+
+            if (n % 2 == 0) {
+                n = n + 1;
+            }
+
+            Console.WriteLine(n);
+            Console.WriteLine(n + 2);
+            Console.WriteLine(n + 4);
+            Console.WriteLine(n + 6);
+            Console.WriteLine(n + 8);
+            Console.WriteLine(n + 10);
+        }
+    }
+}
