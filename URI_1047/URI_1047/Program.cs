9cd1763408a4a5510f30a9f436923e56e65e7ac3
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-09-01 21:22:49 -0300

    Update Program.cs

--- URI_1047/URI_1047/Program.cs
+++ URI_1047/URI_1047/Program.cs
@@ -3,44 +3,27 @@
 namespace URI_1047 {
     class Program {
         static void Main(string[] args) {
-            int horaInicial, minutoInicial, horaFinal, minutoFinal, horas = 0, minutos = 0, resto, tempoTotal = 0;
+            int horaInicial, minutoInicial, horaFinal, minutoFinal;
             string[] vet = Console.ReadLine().Split(' ');
             horaInicial = int.Parse(vet[0]);
             minutoInicial = int.Parse(vet[1]);
             horaFinal = int.Parse(vet[2]);
             minutoFinal = int.Parse(vet[3]);
 
-            if (horaInicial == horaFinal && minutoInicial == minutoFinal) {
-                horas = 24;
-            } else if (horaInicial == horaFinal) {
-                if (minutoInicial > minutoFinal) {
-                    minutos = (60 + minutoFinal) - minutoInicial;
-                    horas = 23;
-                } else {
-                    minutos = minutoFinal - minutoInicial;
-                }
-            } else if (horaInicial < horaFinal) {
-                if (minutoInicial > minutoFinal) {
-                    minutos = minutoFinal - minutoInicial;
-                    tempoTotal = ((horaFinal - horaInicial) * 60) + minutos;
-                } else {
-                    tempoTotal = ((horaFinal - horaInicial) * 60) + (minutoFinal - minutoInicial);
-                }
-                horas = tempoTotal / 60;
-                minutos = tempoTotal % 60;
-            } else if (horaInicial > horaFinal) {
-                if (minutoInicial > minutoFinal) {
-                    minutos = (60 + minutoFinal) - minutoInicial;
-                    tempoTotal = (((23 - horaInicial) + horaFinal)  * 60) + minutos;
-                    horas = tempoTotal / 60;
-                    minutos = tempoTotal % 60;
-                } else {
-                    tempoTotal = (((24 - horaInicial) + horaFinal) * 60) + minutoFinal - minutoInicial;
-                    horas = tempoTotal / 60;
-                    minutos = tempoTotal % 60;
-                }
+            int instanteInicial = horaInicial * 60 + minutoInicial;
+            int instanteFinal = horaFinal * 60 + minutoFinal;
+
+            int duracao;
+            if (instanteInicial <  instanteFinal) {
+                duracao = instanteFinal - instanteInicial;
+            } else {
+                duracao = (24 * 60 - instanteInicial) + instanteFinal;
             }
-            Console.WriteLine("O JOGO DUROU " + horas + " HORA(S) E " + minutos + " MINUTO(S)");
+
+            int duracaoHoras = duracao / 60;
+            int duracaoMinutos = duracao % 60;
+
+            Console.WriteLine("O JOGO DUROU " + duracaoHoras + " HORA(S) E " + duracaoMinutos + " MINUTO(S)");
         }
     }
 }
