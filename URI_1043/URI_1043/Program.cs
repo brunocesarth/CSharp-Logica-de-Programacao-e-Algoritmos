aa159cefa4380118183ad2c8185bea8487a5dedf
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-09-02 18:07:36 -0300

    URI_1043

---
+++ URI_1043/URI_1043/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace URI_1043 {
+    class Program {
+        static void Main(string[] args) {
+            double a, b, c, area, perimentro;
+            string[] vet = Console.ReadLine().Split(' ');
+            a = double.Parse(vet[0], CultureInfo.InvariantCulture);
+            b = double.Parse(vet[1], CultureInfo.InvariantCulture);
+            c = double.Parse(vet[2], CultureInfo.InvariantCulture);
+
+            if (a < b + c && b < c + a && c < a + b) {
+                perimentro = a + b + c;
+                Console.WriteLine("Perimetro = " + perimentro.ToString("F1", CultureInfo.InvariantCulture));
+            } else {
+                area = ((a + b) * c) / 2;
+                Console.WriteLine("Area = " + area.ToString("F1", CultureInfo.InvariantCulture));
+            }
+        }
+    }
+}
