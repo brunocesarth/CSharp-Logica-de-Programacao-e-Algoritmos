7dd65d38f4a6dbf5a44bd252af7bf80d973c1dbd
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-09-03 17:11:02 -0300

    URI_1114

---
+++ URI_1114/URI_1114/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace URI_1114 {
+    class Program {
+        static void Main(string[] args) {
+            int tentativa, senha;
+            tentativa = 0;
+            senha = 2002;
+
+            while (tentativa != senha) {
+                tentativa = int.Parse(Console.ReadLine());
+                if (tentativa != senha) {
+                    Console.WriteLine("Senha Invalida");
+                } else {
+                    Console.WriteLine("Acesso Permitido");
+                }
+            }
+        }
+    }
+}
