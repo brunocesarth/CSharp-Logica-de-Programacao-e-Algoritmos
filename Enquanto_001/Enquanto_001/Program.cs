5e44104fbde17b6ac5965e044f80959093f2bbd3
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-09-03 10:57:16 -0300

    estrutura enquanto

---
+++ Enquanto_001/Enquanto_001/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Enquanto_001 {
+    class Program {
+        static void Main(string[] args) {
+            int x, soma;
+
+            x = int.Parse(Console.ReadLine());
+            soma = 0;
+            while (x != 0) {
+                soma = soma + x;
+                x = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine(soma);
+        }
+    }
+}
