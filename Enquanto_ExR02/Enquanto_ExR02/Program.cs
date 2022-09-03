c9f2a0ef2ea5bca4aacb28498ee9d857721dfea5
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-09-03 16:03:36 -0300

    Exercício 02

---
+++ Enquanto_ExR02/Enquanto_ExR02/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace Enquanto_ExR02 {
+    class Program {
+        static void Main(string[] args) {
+            int n, contador = 0;
+            double result = 0;
+            n = int.Parse(Console.ReadLine());
+
+            if (n > 0) {
+                while (n > 0) {
+                    result += n;
+                    contador += 1;
+                    n = int.Parse(Console.ReadLine());
+                }
+                result = result / contador;
+                Console.WriteLine(result.ToString("F2", CultureInfo.InvariantCulture));
+            } else {
+                Console.WriteLine("impossivel calcular");
+            }
+        }
+    }
+}
