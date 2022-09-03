77f72184da2ae390308000e9fa8cc858489a91a3
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-09-03 19:40:18 -0300

    URI_1154

---
+++ URI_1154/URI_1154/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace URI_1154 {
+    class Program {
+        static void Main(string[] args) {
+            int n, contador;
+            double soma, media;
+            soma = 0.00;
+            media = 0.00;
+            contador = 0;
+
+            n = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            while (n > 0) {
+                soma += n;
+                contador += 1;
+                n = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+            media = soma / contador;
+            Console.WriteLine(media.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
