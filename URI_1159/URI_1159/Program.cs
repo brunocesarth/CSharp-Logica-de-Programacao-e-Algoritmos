b5d35f3c9fc97021d4c5001ad939258a67f59e88
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-09-04 10:38:52 -0300

    Finalização do exercício

--- URI_1159/URI_1159/Program.cs
+++ URI_1159/URI_1159/Program.cs
@@ -5,23 +5,29 @@ namespace URI_1159 {
         static void Main(string[] args) {
             int x, soma, cont;
             x = int.Parse(Console.ReadLine());
-            soma = 0;
 
             while (x != 0) {
                 if (x % 2 == 0) {
                     cont = 0;
+                    soma = 0;
                     while (cont < 5) {
                         soma = soma + x;
+                        x += 2;
                         cont += 1;
                     }
                     Console.WriteLine(soma);
+                    soma = 0;
                 } else {
                     cont = 0;
+                    soma = 0;
+                    x = x + 1;
                     while (cont < 5) {
-                        soma = soma + x + 1;
+                        soma = soma + x;
+                        x += 2;
                         cont += 1;
                     }
                     Console.WriteLine(soma);
+                    soma = 0;
                 }
                 x = int.Parse(Console.ReadLine());
             }
