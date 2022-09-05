a49fa6a64ee394253841223509496bfb90dd9906
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-09-05 11:28:20 -0300

    Estrutura de repetição para

---
+++ Para_001/Para_001/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Para_001 {
+    class Program {
+        static void Main(string[] args) {
+            int n, x, soma = 0;
+            n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++) {
+                x = int.Parse(Console.ReadLine());
+                soma += x;
+            }
+            Console.WriteLine(soma);
+
+            for (int i = 0; i < 5; i++) {
+                Console.WriteLine("Valor de i: " + i);
+            }
+
+            for (int i = 4; i >= 0; i--) {
+                Console.WriteLine("Valor de i: " + i);
+            }
+        }
+    }
+}
