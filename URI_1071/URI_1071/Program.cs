ae9553f7650fcd56d2618a90f856a494d348297c
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-09-06 11:08:05 -0300

    Update Program.cs

--- URI_1071/URI_1071/Program.cs
+++ URI_1071/URI_1071/Program.cs
@@ -3,20 +3,24 @@
 namespace URI_1071 {
     class Program {
         static void Main(string[] args) {
-            int x, y, result = 0;
+            int x, y, result = 0, i, min, max;
             x = int.Parse(Console.ReadLine());
             y = int.Parse(Console.ReadLine());
 
-            if (x > y) {
-                if (x % 2 == 1 || x % 2 == -1) {
-                    for (int i = 0; y < x; i++) {
-                        result += y;
-                        Console.WriteLine(result);
-                    }
-                    Console.WriteLine(result);
-                    y += 1;
+            if (x < y) {
+                min = x;
+                max = y;
+            } else {
+                min = y;
+                max = x;
+            }
+
+            for (i = min + 1; i < max; i++) {
+                if (i % 2 != 0) {
+                    result += i;
                 }
             }
+            Console.WriteLine(result);
         }
     }
 }
