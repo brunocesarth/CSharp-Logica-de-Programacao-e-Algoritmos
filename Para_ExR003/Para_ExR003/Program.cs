1fe3bef07ec6d53de7f864bd537435a90576cc43
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-09-05 21:33:39 -0300

    Exercicios

---
+++ Para_ExR003/Para_ExR003/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Para_ExR003 {
+    class Program {
+        static void Main(string[] args) {
+            int n, result;
+            n = int.Parse(Console.ReadLine());
+            result = 0;
+
+            for (int i = 1; i <= 10; i++) {
+                result = (i * n);
+                Console.WriteLine(i + " x " + n + " = " + result);
+            }
+        }
+    }
+}
---
+++ URI_1071/URI_1071/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace URI_1071 {
+    class Program {
+        static void Main(string[] args) {
+            int x, y, result = 0;
+            x = int.Parse(Console.ReadLine());
+            y = int.Parse(Console.ReadLine());
+
+            if (x > y) {
+                if (x % 2 == 1 || x % 2 == -1) {
+                    for (int i = 0; y < x; i++) {
+                        result += y;
+                        Console.WriteLine(result);
+                    }
+                    Console.WriteLine(result);
+                    y += 1;
+                }
+            }
+        }
+    }
+}
