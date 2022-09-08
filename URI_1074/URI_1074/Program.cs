675ee58858409e588821ed9acc840775976e9668
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-09-08 19:01:29 -0300

    URI_1074

---
+++ URI_1074/URI_1074/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace URI_1074 {
+    class Program {
+        static void Main(string[] args) {
+            int N, X;
+            N = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= N; i++) {
+                X = int.Parse(Console.ReadLine());
+                if (X < 0 && X % 2 == 0) {
+                    Console.WriteLine("EVEN NEGATIVE");
+                } else if (X > 0 && X % 2 == 0) {
+                    Console.WriteLine("EVEN POSITIVE");
+                } else if (X < 0 && X % 2 != 0) {
+                    Console.WriteLine("ODD NEGATIVE");
+                } else if (X > 0 && X % 2 != 0) {
+                    Console.WriteLine("ODD POSITIVE");
+                } else if (X == 0) {
+                    Console.WriteLine("NULL");
+                }
+            }
+        }
+    }
+}
