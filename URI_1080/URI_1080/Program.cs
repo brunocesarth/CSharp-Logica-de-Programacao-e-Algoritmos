3a111a8512bb06b11be24a9d58996b94f6731673
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-09-08 19:27:35 -0300

    URI_1080

---
+++ URI_1080/URI_1080/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace URI_1080 {
+    class Program {
+        static void Main(string[] args) {
+            int x, posicao, n;
+            x = 0;
+            posicao = 0;
+
+            for (int i = 1; i <= 100; i++) {
+                n = int.Parse(Console.ReadLine());
+                if (n > x) {
+                    x = n;
+                    posicao = i;
+                }
+            }
+            Console.WriteLine(x);
+            Console.WriteLine(posicao);
+        }
+    }
+}
