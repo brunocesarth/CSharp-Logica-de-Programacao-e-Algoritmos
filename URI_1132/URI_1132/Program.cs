0a08fc7efe967f94e8f4cc68b6e5a014c4f06239
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-09-09 10:01:23 -0300

    URI_1132

---
+++ URI_1132/URI_1132/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace URI_1132 {
+    class Program {
+        static void Main(string[] args) {
+            int n1, n2, min, max, resultado = 0;
+            n1 = int.Parse(Console.ReadLine());
+            n2 = int.Parse(Console.ReadLine());
+
+            if (n1 > n2) {
+                min = n2;
+                max = n1;
+            } else {
+                min = n1;
+                max = n2;
+            }
+
+            for (int i = min; i <= max; i++) {
+                if (i % 13 != 0) {
+                    resultado += i;
+                }
+            }
+
+            Console.WriteLine(resultado);
+        }
+    }
+}
