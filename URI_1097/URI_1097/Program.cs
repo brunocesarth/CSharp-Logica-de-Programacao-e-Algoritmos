36455759ffce033dce6b3ba913b79f1d818dfdec
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-09-09 09:45:06 -0300

    URI_1097

---
+++ URI_1097/URI_1097/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace URI_1097 {
+    class Program {
+        static void Main(string[] args) {
+            int x, y;
+            x = 5;
+            y = 3;
+
+            for (int i = 1; i <= 9; i += 2) {
+                x += 2;
+                y += 2;
+                for (int j = x; j >= y; j--) {
+                    Console.WriteLine("I=" + i + " " + "J=" + j);
+                }
+                
+            }
+        }
+    }
+}
