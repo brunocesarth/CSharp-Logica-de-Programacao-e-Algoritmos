370f1b77f1fb81a02f000454d70e80f98ca0bd05
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-09-10 16:35:00 -0300

    URI_1155

---
+++ URI_1155/URI_1155/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Globalization;
+
+namespace URI_1155 {
+    class Program {
+        static void Main(string[] args) {
+            double S = 0;
+
+            for (int i = 1; i <= 100; i++) {
+                S += (1.0/i);
+            }
+            Console.WriteLine(S.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
