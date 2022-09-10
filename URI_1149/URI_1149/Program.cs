f1033d25243c44980d5241fc6a876532628b3544
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-09-10 18:24:13 -0300

    URI_1149

---
+++ URI_1149/URI_1149/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace URI_1149 {
+    class Program {
+        static void Main(string[] args) {
+            int a, n, i, soma;
+
+            string[] vet = Console.ReadLine().Split(' ');
+            a = int.Parse(vet[0]);
+
+            i = 1;
+            n = int.Parse(vet[1]);
+            while (n <= 0) {
+                i += 1;
+                n = int.Parse(vet[i]);
+            }
+
+            soma = 0;
+            for (i = 0; i < n; i++) {
+                soma += a + i;
+            }
+
+            Console.WriteLine(soma);
+        }
+    }
+}
