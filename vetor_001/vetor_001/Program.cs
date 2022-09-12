506c299f7e02b9a5d20fa1d1e128e103034b179b
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-09-12 11:42:09 -0300

    vetor 001

---
+++ vetor_001/vetor_001/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace vetor_001 {
+    class Program {
+        static void Main(string[] args) {
+            int n, i;
+            double[] vet;
+
+            n = int.Parse(Console.ReadLine());
+
+            vet = new double[n];
+
+            for (i = 0; i < n; i++) {
+                vet[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+
+            for (i = 0; i < n; i++) {
+                Console.WriteLine(vet[i].ToString("F1", CultureInfo.InvariantCulture));
+                ;
+            }
+        }
+    }
+}
