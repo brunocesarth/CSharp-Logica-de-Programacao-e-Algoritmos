e14fa81a77cb9372ae3ad43dd2cd62214d8264bf
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-09-12 09:44:24 -0300

    Update Program.cs

--- URI_1160/URI_1160/Program.cs
+++ URI_1160/URI_1160/Program.cs
@@ -4,27 +4,34 @@ using System.Globalization;
 namespace URI_1160 {
     class Program {
         static void Main(string[] args) {
-            int T, i, anos;
-            double G1, G2, PA, PB;
+            int T, i, anos, PA, PB, crescA, crescB;
+            double G1, G2;
             string[] vet;
             T = int.Parse(Console.ReadLine());
 
             for (i = 1; i <= T; i++) {
                 vet = Console.ReadLine().Split(' ');
-                PA = double.Parse(vet[0]);
-                PB = double.Parse(vet[1]);
+                PA = int.Parse(vet[0]);
+                PB = int.Parse(vet[1]);
                 G1 = double.Parse(vet[2], CultureInfo.InvariantCulture);
                 G2 = double.Parse(vet[3], CultureInfo.InvariantCulture);
 
-                for (anos = 0; PA <= PB; anos++) {
-                    if (G2 == 0) {
-                        PA += (PA / 100) * G1;
-                    } else {
-                        PA += (PA / 100) * G1;
-                        PB += (PB / 100) * G2;
-                    }
+                crescA = PA;
+                crescB = PB;
+
+                anos = 0;
+
+                while (crescA <= crescB) {
+                    crescA += (int)(crescA * (G1 / 100));
+                    crescB += (int)(crescB * (G2 / 100));
+                    anos += 1;
                 }
-                Console.WriteLine(anos + " anos");
+
+                if (anos > 100) {
+                    Console.WriteLine("Mais de 1 seculo");
+                } else {
+                    Console.WriteLine(anos + " anos.");
+                }
             }
         }
     }
