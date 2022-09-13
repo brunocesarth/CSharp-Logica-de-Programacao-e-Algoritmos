b118fc0b6e409a6bee35c7bc35e3928ef98203b6
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-09-13 10:33:45 -0300

    Update Program.cs

--- Vetor_003/Vetor_003/Program.cs
+++ Vetor_003/Vetor_003/Program.cs
@@ -3,7 +3,19 @@
 namespace Vetor_003 {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            int n;
+            n = int.Parse(Console.ReadLine());
+            string[] vet = new string[n];
+
+            for (int i = 0; i < n; i++) {
+                vet[i] = Console.ReadLine();
+            }
+
+            Console.WriteLine("Nomes lidos:");
+
+            foreach (string elemento in vet) {
+                Console.WriteLine(elemento);
+            }
         }
     }
 }
