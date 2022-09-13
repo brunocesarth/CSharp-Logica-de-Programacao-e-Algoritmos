18e5166321b8ffbf7dc887bb55127346e16b3a0d
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-09-13 10:58:08 -0300

    Vetores 02

---
+++ Vetores_02/Vetores_02/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Vetores_02 {
+    class Program {
+        static void Main(string[] args) {
+            int n, contador = 0;
+            n = int.Parse(Console.ReadLine());
+            
+            int[] vet = new int[n];
+
+            string[] s = Console.ReadLine().Split(' ');
+
+            for (int i = 0; i < n; i++) {
+                vet[i] = int.Parse(s[i]);
+            }
+
+            for (int i = 0; i < n; i++) {
+                if (vet[i] % 2 == 0) {
+                    Console.Write(vet[i] + " ");
+                    contador++;
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine(contador);
+        }
+    }
+}
