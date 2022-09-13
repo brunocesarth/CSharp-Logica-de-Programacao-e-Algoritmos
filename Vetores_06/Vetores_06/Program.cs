2ef4014cccdbedd9a6f13e71ca8c0ca36ffc1b75
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-09-13 17:46:02 -0300

    Vetores 06

---
+++ Vetores_06/Vetores_06/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Vetores_06 {
+    class Program {
+        static void Main(string[] args) {
+            int n, idade = 0;
+            n = int.Parse(Console.ReadLine());
+            string maisVelho = "";
+            string[] nomes = new string[n];
+            int[] idades = new int[n];
+
+            for(int i = 0; i < n; i++) {
+                string[] s = Console.ReadLine().Split(' ');
+                nomes[i] = s[0];
+                idades[i] = int.Parse(s[1]);
+            }
+
+            for (int i = 0; i < n; i++) {
+                if(idade < idades[i]) {
+                    idade = idades[i];
+                    maisVelho = nomes[i];
+                }
+            }
+
+            Console.WriteLine("Pessoa mais velha: " + maisVelho);
+        }
+    }
+}
