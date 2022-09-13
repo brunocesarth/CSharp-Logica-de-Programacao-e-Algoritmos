8d338e1129918f48e22678c46ddad596a9da478e
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-09-12 21:15:04 -0300

    Exercício resolvido vetor

---
+++ Vetor_ExR001/Vetor_ExR001/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Vetor_ExR001 {
+    class Program {
+        static void Main(string[] args) {
+            int n;
+            int[] vet;
+
+            n = int.Parse(Console.ReadLine());
+            vet = new int[n];
+
+            string[] s = Console.ReadLine().Split(' ');
+
+            for (int i = 0; i < n; i++) {
+                vet[i] = int.Parse(s[i]);
+            }
+
+            for (int i = 0; i < n; i++) {
+                if (vet[i] < 0) {
+                    Console.WriteLine(vet[i]);
+                }
+            }
+        }
+    }
+}
