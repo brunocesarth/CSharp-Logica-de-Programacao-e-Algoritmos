deeb1a2e858dfbd6accbe946a612c3df1db35ae7
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-09-13 10:15:49 -0300

    Aulas de vetores

---
+++ Vetor_ExR003/Vetor_ExR003/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Globalization;
+
+namespace Vetor_ExR003 {
+    class Program {
+        static void Main(string[] args) {
+            int n;
+            n = int.Parse(Console.ReadLine());
+
+            string[] nomes = new string[n];
+            int[] idades = new int[n];
+            double[] alturas = new double[n];
+
+            // Leitura dos dados
+            for (int i = 0; i < n; i++) {
+                string[] s = Console.ReadLine().Split(' ');
+                nomes[i] = s[0];
+                idades[i] = int.Parse(s[1]);
+                alturas[i] = double.Parse(s[2], CultureInfo.InvariantCulture);
+            }
+
+            // Calculo da altura media das pessoas
+            double soma = 0.0;
+            for (int i = 0; i < n; i++) {
+                soma += alturas[i];
+            }
+            double media = soma / n;
+            Console.WriteLine("Altura media: " + media.ToString("F2", CultureInfo.InvariantCulture));
+
+            // Porcentagem de pessoas com menos de 16
+            int cont = 0;
+            for (int i = 0; i < n; i++) {
+                if (idades[i] < 16) {
+                    cont++;
+                }
+            }
+            double porcentagem = (double) cont / n * 100.0;
+            Console.WriteLine("Pessoas com menos de 16 anos: " + porcentagem.ToString("F1", CultureInfo.InvariantCulture) + "%");
+        }
+    }
+}
