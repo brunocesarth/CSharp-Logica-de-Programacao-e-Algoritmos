00d8ad72bb1d3351a68108252524925f25c24ee3
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-09-13 17:16:54 -0300

    Vetores 05

---
+++ Vetores_05/Vetores_05/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace Vetores_05 {
+    class Program {
+        static void Main(string[] args) {
+            int n, contador = 0;
+            double soma, media ;
+            n = int.Parse(Console.ReadLine());
+
+            int[] vet = new int[n];
+
+            string[] s = Console.ReadLine().Split(' ');
+
+            for (int i = 0; i < n; i++) {
+                vet[i] = int.Parse(s[i]);
+            }
+
+            soma = 0.0;
+
+            for (int i = 0; i < n; i++) {
+                if (vet[i] % 2 == 0) {
+                    soma += (double)vet[i];
+                    contador++;
+                }
+            }
+            media =(double) soma / contador;
+
+            Console.WriteLine(media.ToString("F1", CultureInfo.InvariantCulture));
+        }
+    }
+}
