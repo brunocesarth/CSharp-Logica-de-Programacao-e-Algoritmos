d88448093d671fc30b84a9a15d4e5a76af69da26
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-09-14 09:12:31 -0300

    Matriz 001

---
+++ Matriz_001/Matriz_001/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Matriz_001 {
+    class Program {
+        static void Main(string[] args) {
+            int M, N;
+            int[,] A;
+
+            string[] s = Console.ReadLine().Split(' ');
+            M = int.Parse(s[0]);
+            N = int.Parse(s[1]);
+
+            A = new int[M, N];
+
+            for (int i = 0; i < M; i++) {
+                s = Console.ReadLine().Split(' ');
+                for (int j = 0; j < N; j++) {
+                    A[i, j] = int.Parse(s[j]);
+                }
+            }
+
+            for (int i = 0; i < M; i++) {
+                for (int j = 0; j < N; j++) {
+                    Console.Write(A[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
