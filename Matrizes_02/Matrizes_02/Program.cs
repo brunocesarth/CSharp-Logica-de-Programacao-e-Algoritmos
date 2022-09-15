7799cd2ae01aa9e8472f84426aa05c6f393d87a3
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-09-14 21:27:55 -0300

    Matrizes 02

---
+++ Matrizes_02/Matrizes_02/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Matrizes_02 {
+    class Program {
+        static void Main(string[] args) {
+            int N, i, j;
+            int[,] matriz;
+            N = int.Parse(Console.ReadLine());
+
+            matriz = new int[N, N];
+
+            for (i = 0; i < N; i++) {
+                string[] s = Console.ReadLine().Split(' ');
+                for (j = 0; j < N; j++) {
+                    matriz[i, j] = int.Parse(s[j]);
+                }
+            }
+
+            int[] vetor = new int[N];
+
+            for(i = 0; i < N; i++) {
+                int soma = 0;
+                for(j = 0; j < N; j++) {
+                    soma += matriz[i, j];
+                }
+                vetor[i] = soma;
+            }
+
+            for (i = 0; i<N; i++) {
+                Console.WriteLine(vetor[i]);
+            }
+        }
+    }
+}
