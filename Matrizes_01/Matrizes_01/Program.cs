1a9c4366e5ad925f5b896b2f4129ce5bf331357d
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-09-14 21:21:24 -0300

    Matrizes 01

---
+++ Matrizes_01/Matrizes_01/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Matrizes_01 {
+    class Program {
+        static void Main(string[] args) {
+            int M, N, i, j;
+            int[,] matriz;
+
+            string[] s = Console.ReadLine().Split(' ');
+            M = int.Parse(s[0]);
+            N = int.Parse(s[1]);
+
+            matriz = new int[M, N];
+
+            for (i = 0; i < M; i++) {
+                s = Console.ReadLine().Split(' ');
+                for (j = 0; j < N; j++) {
+                    matriz[i, j] = int.Parse(s[j]);
+                }
+            }
+
+
+            Console.WriteLine("VALORES NEGATIVOS:");
+            for (i = 0; i < M; i++) {
+                for (j = 0; j < N; j++) {
+                    if (matriz[i, j] < 0) {
+                        Console.WriteLine(matriz[i,j]);
+                    }
+                }
+            }
+
+
+        }
+    }
+}
